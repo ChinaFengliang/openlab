3c10ad4f5651fc6e33e9ae021217001d1f5dac44
冯梁 <chinafengliang@aliyun.com>
2020-07-21 19:08:56 +0800

    bmc: 使能 account, password 字段

--- bmc.py
+++ bmc.py
@@ -2,43 +2,38 @@
 
 import os
 
-
-def bind_device(ip):
-    cmdline = 'ipmitool -H %s -I lanplus -U root -P Huawei12#$\
-        sol activate' % ip
+def bind_device(ip, account, password):
+    cmdline = 'ipmitool -H %s -I lanplus -U %s -P %s\
+        sol activate' % (ip, account, password)
     os.system(cmdline)
     return 0
 
-
-def unbind_device(ip):
-    cmdline = 'ipmitool -H %s -I lanplus -U root -P Huawei12#$\
-        sol deactivate' % ip
+def unbind_device(ip, account, password):
+    cmdline = 'ipmitool -H %s -I lanplus -U %s -P %s\
+        sol deactivate' % (ip, account, password)
     os.system(cmdline)
     return 0
 
-
-def reset_device(ip):
-    cmdline = 'ipmitool -H %s -I lanplus -U root -P Huawei12#$\
-        chassis power reset' % ip
+def reboot_device(ip, account, password):
+    cmdline = 'ipmitool -H %s -I lanplus -U %s -P %s\
+        chassis power reset' % (ip, account, password)
     os.system(cmdline)
     return 0
 
-
-def poweron_device(ip):
-    cmdline = 'ipmitool -H %s -I lanplus -U root -P Huawei12#$\
-        chassis power on' % ip
+def poweron_device(ip, account, password):
+    cmdline = 'ipmitool -H %s -I lanplus -U %s -P %s\
+        chassis power on' % (ip, account, password)
     os.system(cmdline)
     return 0
 
-
-def poweroff_device(ip):
-    cmdline = 'ipmitool -H %s -I lanplus -U root -P Huawei12#$\
-        chassis power off' % ip
+def poweroff_device(ip, account, password):
+    cmdline = 'ipmitool -H %s -I lanplus -U %s -P %s\
+        chassis power off' % (ip, account, password)
     os.system(cmdline)
     return 0
 
-def device_status(ip):
-    cmdline = 'ipmitool -H %s -I lanplus -U root -P Huawei12#$\
-        chassis status' % ip
+def device_status(ip, account, password):
+    cmdline = 'ipmitool -H %s -I lanplus -U %s -P %s\
+        chassis status' % (ip, account, password)
     os.system(cmdline)
     return 0
